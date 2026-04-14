7b039fe54fddee603a12f736f2cf25a5143903de
radiolab <48355256+radiolab81@users.noreply.github.com>
2026-04-14 14:06:15 +0000

    Update build_cohiplayers.sh

--- build_cohiplayers.sh
+++ build_cohiplayers.sh
@@ -2,5 +2,4 @@
 sudo apt install libncurses5-dev libncursesw5-dev libliquid-dev
 g++ -O3 -o cohiplayer_smi_cmdline cohiplayer_smi_cmdline.cpp -lliquid -lm -lncurses
 g++ -O3 -o cohiplayer_smi_tui cohiplayer_smi_tui.cpp -lliquid -lm -lncurses
-
-
+g++ -O3 -o cohiplayer_smi_tui_mt cohiplayer_smi_tui_mt.cpp -lliquid -lm -lncurses
